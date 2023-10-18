8e68829ace169bb5aba1dcb16de3a1131a4b7f97
Clément Lemoine <clementlemoine.cl@gmail.com>
2023-10-18 11:31:13 +0200

    🎉 basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,70 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const netlifyEnv = process.env.NETLIFY_ENV || "development";
+const isPreview = process.env.PREVIEW === "true";
+const URL =
+    process.env.NETLIFY_ENV === "production"
+        ? process.env.URL
+        : process.env.DEPLOY_PRIME_URL;
+const isDevEnv = netlifyEnv === "development";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    runtimeConfig: {
+        public: {
+            baseUrl: URL || "http://localhost:8888",
+            preview: isPreview,
+        },
+    },
+    app: {
+        head: {
+            meta: [
+                {
+                    charset: "utf-8",
+                },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1.0",
+                },
+            ],
+            link: [
+                {
+                    rel: "icon",
+                    href: "/favicon.ico",
+                    sizes: "any",
+                },
+                {
+                    rel: "icon",
+                    href: "/icon.svg",
+                    type: "image/svg+xml",
+                },
+                {
+                    rel: "apple-touch-icon",
+                    href: "/apple-touch-icon.png",
+                },
+                {
+                    rel: "manifest",
+                    href: "/manifest.webmanifest",
+                },
+            ],
+        },
+    },
+    nitro: {
+        prerender: {
+            crawlLinks: true,
+            failOnError: false,
+        },
+    },
+    css: ["@/assets/scss/main.scss"],
+    build: {
+        transpile: ["gsap"],
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData:
+                        '@use "sass:math";@import "@/assets/scss/base/_variables.scss";',
+                },
+            },
+        },
+    },
+});
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="page">TICA</div>
+</template>
+
+<script setup>
+// import query from "@/cms/queries/home";
+
+// const locale = useI18n().locale.value;
+
+// const { data, error } = await useDato({
+//     query,
+//     variables: { locale },
+//     key: "home",
+// });
+
+// if (error.value || !data.value) {
+//     throw showError({ statusCode: 404, statusMessage: "Page Not Found" });
+// }
+
+// useSeo(data.value?.home?._seoMetaTags);
+</script>
+
+<style lang="scss" scoped></style>
