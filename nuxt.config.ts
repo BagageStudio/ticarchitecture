76d995f748c3f0531e61307d7a04a05188ab7698
Clément Lemoine <clementlemoine.cl@gmail.com>
2023-10-19 15:20:33 +0200

    🍱 font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,17 @@ export default defineNuxtConfig({
                     rel: "manifest",
                     href: "/manifest.webmanifest",
                 },
+
+                { rel: "preconnect", href: "https://fonts.googleapis.com" },
+                {
+                    rel: "preconnect",
+                    href: "https://fonts.gstatic.com",
+                    crossorigin: "",
+                },
+                {
+                    href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap",
+                    rel: "stylesheet",
+                },
             ],
         },
     },
